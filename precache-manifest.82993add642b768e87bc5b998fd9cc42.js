c259df1df9c3fab14ad8b23aca30139c8c6d181e
neeldexter <neelamdas83@gmail.com>
2020-06-17 01:58:19 +0530

    Updates

---
+++ precache-manifest.82993add642b768e87bc5b998fd9cc42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbfdf28500eb2798e7e35ded5ad8efb",
+    "url": "/covidApp/index.html"
+  },
+  {
+    "revision": "2aba53ddeeb5b1e4d4f7",
+    "url": "/covidApp/static/css/main.38eac72b.chunk.css"
+  },
+  {
+    "revision": "12537db3eff8bcdb0150",
+    "url": "/covidApp/static/js/2.ba3e6a2b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidApp/static/js/2.ba3e6a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aba53ddeeb5b1e4d4f7",
+    "url": "/covidApp/static/js/main.c4635334.chunk.js"
+  },
+  {
+    "revision": "fc007c038a1b39c4aff6",
+    "url": "/covidApp/static/js/runtime-main.408e0fbc.js"
+  },
+  {
+    "revision": "8a7ba9757293eeeee5e5e3b9fa4f74f0",
+    "url": "/covidApp/static/media/COVID.8a7ba975.png"
+  }
+]);
